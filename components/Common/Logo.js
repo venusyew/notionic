df9857cf388f49d01bc6c4282ba4ac724de73534
Dakki <yewzheexiu@gmail.com>
2023-08-27 10:41:24 +0800

    Update Logo.js

--- components/Common/Logo.js
+++ components/Common/Logo.js
@@ -2,20 +2,9 @@
 import * as React from 'react'
 
 const Logo = (props) => (
-  <svg
-    xmlns='http://www.w3.org/2000/svg'
-    width='24'
-    height='24'
-    viewBox='0 0 100 100'
-    style={{
-      opacity: 1
-    }}
-    {...props}
-  >
-    
+ 
       <img src="https://raw.githubusercontent.com/venusyew/notionic/main/public/favicon.png">
    
-  </svg>
 )
 
 export default Logo
