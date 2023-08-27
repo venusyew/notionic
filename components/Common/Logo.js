490132947d31c975edb5739edb24e4fe919b6c80
Dakki <yewzheexiu@gmail.com>
2023-08-27 11:48:15 +0800

    Update Logo.js

--- components/Common/Logo.js
+++ components/Common/Logo.js
@@ -3,28 +3,7 @@ import * as React from 'react'
 
 const Logo = (props) => (
  
-      <svg
-    xmlns='http://www.w3.org/2000/svg'
-    width='24'
-    height='24'
-    viewBox='0 0 100 100'
-    style={{
-      opacity: 1
-    }}
-    {...props}
-  >
- <g transform="translate(0.000000,24.000000) scale(0.50000,0.50000)"
- stroke="none">
-<path d="M34 185 c-4 -11 -14 -28 -22 -37 -14 -16 -12 -18 17 -24 22 -5 30
--11 27 -20 -7 -19 9 -18 24 1 7 9 18 14 25 11 6 -2 17 5 23 15 15 23 9 24 -23
-3 -23 -15 -24 -15 -22 13 1 15 7 31 12 35 6 5 6 8 -2 8 -6 0 -15 -5 -18 -11
--5 -6 -12 -4 -20 8 -13 17 -14 17 -21 -2z"/>
-<path d="M115 89 c-4 -6 -5 -12 -2 -15 2 -3 7 2 10 11 7 17 1 20 -8 4z"/>
-<path d="M198 70 c-22 -39 -51 -41 -43 -2 6 24 5 25 -8 10 -7 -10 -26 -22 -40
--27 -15 -6 -27 -15 -27 -21 0 -8 6 -8 20 0 14 7 26 8 39 0 44 -23 73 -9 86 43
-9 36 -6 34 -27 -3z"/>
-</g>
-  </svg>
+    <img src="https://github.com/venusyew/notionic/blob/main/public/favicon.png?raw=true"/>
 )
 
 export default Logo
