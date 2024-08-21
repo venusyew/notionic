f109391d1d0881cc394d872a3fff373930fbd249
Dakki <yewzheexiu@gmail.com>
2024-08-22 01:12:03 +0800

    Update ThemeSwitcher.js

--- components/NavBar/ThemeSwitcher.js
+++ components/NavBar/ThemeSwitcher.js
@@ -19,7 +19,7 @@ const ThemeSwitcher = () => {
             theme === 'light' ? 'dark' : theme === 'system' ? 'dark' : 'light'
           )
         }
-        className='p-2 ml-1 hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer rounded-lg dark:text-gray-100'
+        className='p-2 ml-1 hover:bg-neutral-200 dark:hover:bg-neutral-700 cursor-pointer rounded-lg dark:text-yellow-100'
       >
         {hasMounted && theme === 'dark' ? (
           <MoonIcon className='h-5 w-5' />
