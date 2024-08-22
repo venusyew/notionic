faed96f68d9dca39f6f99d3fab067903e9dd6a3c
Dakki <yewzheexiu@gmail.com>
2024-08-22 16:32:19 +0800

    Update PostFooter.js

--- components/Post/PostFooter.js
+++ components/Post/PostFooter.js
@@ -13,8 +13,8 @@ const PostFooter = () => {
   const t = lang[locale]
 
   return (
-    <div className='w-full pb-12 justify-between font-medium text-gray-500 dark:text-gray-400'>
-      <div className='flex flex-wrap sm:flex-nowrap sm:justify-between items-center justify-center rounded-lg bg-gray-100 dark:bg-gray-700 relative gap-3 px-4 py-3'>
+    <div className='w-full pb-12 justify-between font-medium text-neutral-500 dark:text-neutral-400'>
+      <div className='flex flex-wrap sm:flex-nowrap sm:justify-between items-center justify-center rounded-lg bg-neutral-100 dark:bg-neutral-700 relative gap-3 px-4 py-3'>
         <div className='w-full sm:w-auto max-w-screen-sm inline-block text-sm font-light md:text-base mb-2 sm:mb-0'>
           {t.LAYOUT.NOTICE_TEXT}
         </div>
@@ -22,7 +22,7 @@ const PostFooter = () => {
           {BLOG.showWeChatPay && (
             <button
               onClick={() => setShowPay((showPay) => !showPay)}
-              className='flex gap-1 bg-gray-200 dark:bg-gray-600 hover:bg-gray-300 dark:hover:bg-gray-500 hover:text-gray-600 dark:hover:text-gray-300 text-sm rounded-lg px-4 py-2'
+              className='flex gap-1 bg-gray-200 dark:bg-neutral-600 hover:bg-neutral-300 dark:hover:bg-neutral-500 hover:text-neutral-600 dark:hover:text-neutral-300 text-sm rounded-lg px-4 py-2'
             >
               <ThumbUpIcon className='flex flex-col justify-center items-center select-none cursor-pointer relative w-5 h-5' />
               {t.LAYOUT.PAY_BUTTON}
@@ -30,7 +30,7 @@ const PostFooter = () => {
           )}
           <button
             onClick={() => router.push(BLOG.path || '/contact')}
-            className='flex gap-1 bg-gray-200 dark:bg-gray-600 hover:bg-gray-300 dark:hover:bg-gray-500 hover:text-gray-600 dark:hover:text-gray-300 text-sm rounded-lg px-4 py-2'
+            className='flex gap-1 bg-neutral-200 dark:bg-neutral-600 hover:bg-neutral-300 dark:hover:bg-neutral-500 hover:text-neutral-600 dark:hover:text-neutral-300 text-sm rounded-lg px-4 py-2'
           >
             <MailIcon className='flex flex-col justify-center items-center select-none cursor-pointer relative w-5 h-5' />
             {t.LAYOUT.NOTICE_BUTTON}
