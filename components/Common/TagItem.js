9de21e0641f96eb4bb796b2ae9437900cbf5ad12
Dakki <yewzheexiu@gmail.com>
2024-08-22 16:50:41 +0800

    Update TagItem.js

--- components/Common/TagItem.js
+++ components/Common/TagItem.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const TagItem = ({ tag }) => (
   <Link href={`/tag/${encodeURIComponent(tag)}`} scroll={false}>
-    <p className='mr-2 rounded-full px-2 py-1 bg-neutral-200 dark:bg-neutral-700 hover:bg-neutral-300 dark:hover:bg-neutral-600 leading-none text-sm'>
+    <p className='mr-2 rounded-full px-2 py-1 bg-stone-200 dark:bg-stone-700 hover:bg-stone-300 dark:hover:bg-stone-600 leading-none text-sm'>
       {tag}
     </p>
   </Link>
